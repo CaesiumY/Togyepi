2f6d280c05268d7db2891e09e874bc51e61f5cba
caesiumy <dbs2636@gmail.com>
2021-09-22 22:52:48 +0900

    feat: Add calc autocomplete input

--- src/components/CalculatorContainer.vue
+++ src/components/CalculatorContainer.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>calc container</div>
+  <section class="d-flex flex-column justify-center text-center ma-5">
+    <article class="mt-10">
+      <v-autocomplete
+        v-model="selectedFormula"
+        auto-select-first
+        clearable
+        rounded
+        solo-inverted
+        label="공식을 선택하세요"
+        :items="formulaNames"
+      ></v-autocomplete>
+    </article>
+  </section>
 </template>
 
 <script>
@@ -10,6 +22,7 @@ export default {
     formulaNames: Object.keys(calculatingFormula).map(
       (f) => calculatingFormula[f].name
     ),
+    selectedFormula: "",
   }),
   mounted() {
     console.log(this.formula);
