e0cd70b68f8b79f58ebd4eba438d0871c1af3437
caesiumy <dbs2636@gmail.com>
2021-10-04 22:30:27 +0900

    Updates

---
+++ precache-manifest.27cc05a1109e1891c76e9fd6e0bc1f81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e96ecde69408186fbb",
+    "url": "/Togyepi/css/app.4b31bd02.css"
+  },
+  {
+    "revision": "cfef359d1c3dc027d46a",
+    "url": "/Togyepi/css/chunk-vendors.1ae6e1ee.css"
+  },
+  {
+    "revision": "eb2c9e69c65bc719551daae36fab3773",
+    "url": "/Togyepi/img/logo_only.eb2c9e69.png"
+  },
+  {
+    "revision": "10e2ea7f87f497fec1693e2e3649f8ce",
+    "url": "/Togyepi/index.html"
+  },
+  {
+    "revision": "b9e96ecde69408186fbb",
+    "url": "/Togyepi/js/app.a5711cc3.js"
+  },
+  {
+    "revision": "cfef359d1c3dc027d46a",
+    "url": "/Togyepi/js/chunk-vendors.3bc4b1c1.js"
+  },
+  {
+    "revision": "5ee0978db4bc06c56fec273f714a6851",
+    "url": "/Togyepi/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Togyepi/robots.txt"
+  }
+]);
